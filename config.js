56c69f1f190789d84d62362ebcc9b6621b098da4
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-12-19 16:42:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,11 +16,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['62895604670507']
-global.ownernomer = "62895604670507"
-global.premium = ['62895604670507','6281328144300']
-global.packname = 'Zenss'
-global.author = 'Cuyy'
+global.owner = ['6285624823115']
+global.ownernomer = "6285624823115"
+global.premium = ['6285624823115','6281646829895']
+global.packname = 'Irfaan'
+global.author = 'Official'
 global.sessionName = 'zenss'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
